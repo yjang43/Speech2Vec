d45acaaffd78ed31db3b48020b8f6f4098c42573
yjang43 <yjang43@wisc.edu>
2022-06-20 19:52:18 -0500

    Dataset fast version

--- dataset.py
+++ dataset.py
@@ -14,21 +14,27 @@ class LibriSpeechDataset(Dataset):
     def __init__(self, data_dir, word_dir, window_sz=3):
         self.data_dir = data_dir
         self.window_sz = window_sz
-        self.npzs = [fn for fn in os.listdir(data_dir) if fn[-4:] == '.npz']
-        txts = [fn for fn in os.listdir(word_dir) if fn[-4:] == '.txt']
+        self.npzs = sorted([fn for fn in os.listdir(data_dir) if fn[-4:] == '.npz'])
+        txts = sorted([fn for fn in os.listdir(word_dir) if fn[-4:] == '.txt'])
         self.ptrs = [0]
         self.words = []
                 
         ptr_cnt = 0
 
-        for npz in tqdm(self.npzs):
+        npz_progbar = tqdm(self.npzs)
+        txt_progbar = tqdm(txts)
+        for npz in npz_progbar:
             word_cnt = np.load(os.path.join(data_dir, npz))['word_cnt']
             ptr_cnt += word_cnt
             self.ptrs.append(ptr_cnt)
-        for txt in tqdm(txts):
+            # npz_progbar.set_description(f"file: {npz}")
+            # npz_progbar.refresh()
+        for txt in txt_progbar:
             with open(os.path.join(word_dir, txt), 'r') as f:
                 ws = f.read()
                 self.words += ws.split()
+            # txt_progbar.set_description(f"file: {txt}")
+            # txt_progbar.refresh()
     
     def __len__(self):
         return self.ptrs[-1] - 2 * self.window_sz
@@ -76,6 +82,78 @@ class LibriSpeechDataset(Dataset):
         tgts = []
         tgt_words = []
         for i in range(2 * self.window_sz):
+            tgt = [item['tgts'][i] for item in batch]
+            # tgt = nn.utils.rnn.pack_sequence(tgt, enforce_sorted=False)
+            # tgt = nn.utils.rnn.pad_sequence(tgt)
+            tgt_word = [item['tgt_words'][i] for item in batch]
+            
+            tgts.append(tgt)
+            tgt_words.append(tgt_word)
+
+        return src, tgts, src_word, tgt_words
+
+    
+
+class LibriSpeechDatasetFast(Dataset):
+    """This implementation loads data in memory. Temporary remedy to slow .npz load.
+    """
+    def __init__(self, data_dir, word_dir, window_sz=3):
+        self.data_dir = data_dir
+        self.window_sz = window_sz
+        self.npzs = sorted([fn for fn in os.listdir(data_dir) if fn[-4:] == '.npz'])
+        txts = sorted([fn for fn in os.listdir(word_dir) if fn[-4:] == '.txt'])
+        
+        self.datas = []
+        self.words = []
+
+        npz_progbar = tqdm(self.npzs)
+        txt_progbar = tqdm(txts)
+        for npz in npz_progbar:
+            data = np.load(os.path.join(data_dir, npz))
+            word_cnt = data['word_cnt']
+            for i in range(word_cnt):
+                d = data[f'arr_{i}']
+                d = mfcc(d, samplerate=16000, winlen=0.025, winstep=0.01, numcep=13)
+                d = torch.from_numpy(d).float()
+                self.datas.append(d)
+            # npz_progbar.set_description(f"file: {npz}")
+            # npz_progbar.refresh()
+        for txt in txt_progbar:
+            with open(os.path.join(word_dir, txt), 'r') as f:
+                ws = f.read()
+                self.words += ws.split()
+            # txt_progbar.set_description(f"file: {txt}")
+            # txt_progbar.refresh()
+    
+    def __len__(self):
+        return len(self.words) - 2 * self.window_sz
+    
+    def __getitem__(self, idx):
+
+        ds = self.datas[idx: idx + 2 * self.window_sz + 1]
+        ws = self.words[idx: idx + 2 * self.window_sz + 1]
+
+            
+        src, tgts = ds[self.window_sz], ds[: self.window_sz] + ds[self.window_sz + 1: ]
+        src_word, tgt_words = ws[self.window_sz], ws[: self.window_sz] + ws[self.window_sz + 1: ]
+        
+        return {'src': src, 'tgts': tgts, 
+                'src_word': src_word, 'tgt_words': tgt_words}
+    
+
+    # collate function
+    # TODO: PAD vs PACKING be sure of the differences!
+    # might need to remember the length of each data
+    def pad_collate(self, batch):
+        src = [item['src'] for item in batch]
+        # src = nn.utils.rnn.pack_sequence(src, enforce_sorted=False)
+        # src = nn.utils.rnn.pad_sequence(src)
+        src_word = [item['src_word'] for item in batch]
+
+        tgts = []
+        tgt_words = []
+        for i in range(2 * self.window_sz):
+            self.x = batch[1]['tgts']
             tgt = [item['tgts'][i] for item in batch]
             # tgt = nn.utils.rnn.pack_sequence(tgt, enforce_sorted=False)
             # tgt = nn.utils.rnn.pad_sequence(tgt)
